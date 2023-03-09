9f5b667e7e9b7b556af7ed3aadf2fbf7a9fd18d9
giorgia-mineo <giomineo@gmail.com>
2023-03-09 12:52:50 +0000

    Basic styling for dictionary complete

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,8 +1,64 @@
+import { useState } from 'react'
+// import { handleChange } from './helpers/functions'
+
+const Dictionary = ({ searchValue, relatedWords, word, audio, pronunciation, wordClass, definition, image }) => {
+
+  // const [ inputValue, setInputValue ] = useState('')
+
+  // const handleChange = (e) => {
+  //   setInputValue(e.target.value)
+  //   const searchValue = inputValue
+  //   console.log(inputValue)
+  //   return searchValue
+  // }
 
 
-const Dictionary = () => {
   return (
-    console.log('x')
+    <main>
+      {/* SIDEBAR */}
+      <div className="sidebar">
+        {relatedWords}
+      </div>
+
+      {/* MAIN */}
+      <section>
+        {/* ENTRY */}
+        <div className="wrapper">
+          {/* <div className="searchbar">
+            <label htmlFor="search"></label>
+            <input type="text" name="search" placeholder="Type here" onChange={handleChange} value={searchValue} />
+          </div> */}
+
+          <div className="word-container">
+
+            <div className="word-and-audio">
+              <img className="icons" alt='img' />
+              <div className="title">
+                <h2>{word}</h2>
+              </div>
+              <div className="icons" >▶️
+                <audio src={audio}></audio>
+              </div>
+            </div>
+
+            <div className="pronunciation-and-class">
+              <h4>{pronunciation}</h4>
+              <h4>{wordClass}</h4>
+            </div>
+
+            <div className="definition">
+              <h4>Definition</h4>
+              <p>{definition}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* ILLUSTRATION */}
+        <div className="illustration">
+          <img src={image} alt="img"></img>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/components/School.js
+++ src/components/School.js
@@ -1,31 +1,61 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Dictionary from './Dictionary'
 
 const School = () => {
 
   // ENDPOINTS
   // ids, audio, definition, illustration, word, pronunciation, class
 
-  const [ data, setData ] = useState([])
+  const [ entry, setEntry ] = useState([])
+  const [ inputValue, setInputValue ] = useState('')
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+    console.log(inputValue)
+  }
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('https://www.dictionaryapi.com/api/v3/references/sd4/json/school?key=33cbba70-f662-4f2e-9dc8-e2df5cc5ea95')
-      setData(data)
-      console.log(data)
+      try {
+        const { data } = await axios.get('https://www.dictionaryapi.com/api/v3/references/sd4/json/school?key=33cbba70-f662-4f2e-9dc8-e2df5cc5ea95')
+        // console.log(data[0].hwi.prs[0].sound.audio)
+        setEntry(data)
+      } catch (err) {
+        console.log(err)
+      }
     }
     getData()
-  })
+  }, [])
 
   return (
     <>
-      <div>ID: {data[0].meta.id}</div>
-      <div>Audio: {data[0].hwi.prs[0].sound.audio}</div>
-      <div>Definition: {data[0].def[0].sseq[0][0][1].dt[0][1]}</div>
-      <div>Illustration: {}</div>
-      <div>Word: {data[0].hwi.hw}</div>
-      <div>Pronunciation: {data[0].hwi.prs[0].mw}</div>
-      <div>Class: {data[0].fl}</div>
+      { entry.length > 0 &&
+      <>
+        <section className='search-section'>
+          <div className="searchbar">
+            <label htmlFor="search"></label>
+            <input type="text" name="search" placeholder="Type here" onChange={handleChange} value={inputValue} />
+          </div>
+        </section>
+        <Dictionary 
+          relatedWords={'check'}
+          word={entry[0].hwi.hw} 
+          audio={entry[0].hwi.prs[0].sound.audio} 
+          pronunciation={entry[0].hwi.prs[0].mw} 
+          wordClass={entry[0].fl} 
+          definition={entry[0].def[0].sseq[0][0][1].dt[0][1]}
+          image={'http://placekitten.com/200/300'} 
+        />
+        {/* <div>ID: {entry[0].meta.id}</div>
+        <div>Audio: {entry[0].hwi.prs[0].sound.audio}</div>
+        <div>Definition: {entry[0].def[0].sseq[0][0][1].dt[0][1]}</div>
+        <div>Illustration: {}</div>
+        <div>Word: {entry[0].hwi.hw}</div>
+        <div>Pronunciation: {entry[0].hwi.prs[0].mw}</div>
+        <div>Class: {entry[0].fl}</div>  */}
+      </>
+      }
     </>
   )
 }
